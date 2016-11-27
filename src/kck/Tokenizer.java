fa963a5f78d91f9ab5abd2d02cbcbb7c51902103
KCK2016 <adrian-1212@wp.pl>
2016-11-27 13:50:15 +0100

    Parser

--- src/kck/Tokenizer.java
+++ src/kck/Tokenizer.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.regex.Pattern;
 import java.util.LinkedList;
 import java.util.regex.Matcher;
+import java.text.Collator;
+import java.util.*;
+
 
 public class Tokenizer
 {
@@ -79,14 +82,45 @@ public class Tokenizer
     public void addtoken() throws IOException {
 
         File token_txt = new File("dictionary.txt");
-        List<String> token_txt_list = Files.readAllLines(token_txt.toPath());
+        List<String> token_txt_list = new LinkedList<>();
+        token_txt_list = Files.readAllLines(token_txt.toPath());
+
+
+        // sortowanie po dlufgosci pierwszego slowa z lini z pliku dictionary.txt
+
+        token_txt_list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String[] text1 = o1.split(",");
+                String[] text2 = o2.split(",");
+                if(text1[0].length() > text2[0].length()) return -1;
+                else return 1;
+            }
+        });
+
+
+
         for(String line: token_txt_list) {
             String[] line_split = line.trim().split(",");
+
+            // sortowanie tokentow wewnatrz lini
+            if (line_split.length>2) {
+                for (int i = 2; i < line_split.length-1; i++) {
+                    if (line_split[i].length() < line_split[i + 1].length()) {
+                        String temp = line_split[i];
+                        line_split[i] = line_split[i + 1];
+                        line_split[i + 1] = temp;
+                    }
+                }
+            }
+
+
             for (int counter = 2; counter<line_split.length; counter++){
                 add(line_split[counter],Integer.parseInt(line_split[1]), line_split[0]);
             }
         }
         add(",", 12, ",");
+        add("[a-zA-Z][a-zA-Z0-9_]*", 99,"[a-zA-Z][a-zA-Z0-9_]*");
 
     }
 
