a4e2a54d1ed236e9f86c76e46ecffb0717695635
bofuam <bofuam>
2016-12-19 15:48:42 +0100

    added dishoftheday to gui

--- src/kck/KCKParser.java
+++ src/kck/KCKParser.java
@@ -1,6 +1,8 @@
 package kck;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Random;
 
 /**
  * Created by s416094 on 05.12.2016.
@@ -35,5 +37,25 @@ public class KCKParser {
         return tokenizer;
     }
 
+    public String getDishOfTheDay(String group) throws IOException {
+        MenuList menuList = new MenuList();
+        LinkedList<String> listOf;
+        listOf = new LinkedList<String>();
+
+        for (MenuList.Menu tok : menuList.getMenu())
+        {
+            if (tok.group.equals(group) == true)
+            {
+                int i = 0;
+                listOf.add(i, tok.getProduct());
+                i = i + 1;
+            }
+        }
+
+        Random random = new Random();
+        int index = random.nextInt(listOf.size());
+        String item = listOf.get(index);
+       return item;
+    }
 
 }
