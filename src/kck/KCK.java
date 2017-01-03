63e8108f976e82b2a50f6e44e5675c59f832029f
KCK2016 <adrian-1212@wp.pl>
2017-01-03 17:16:57 +0100

    1.Większe pokrycie klasy Waiter. 2.Nie zawiesza się przy dawaniu "?, !, ." na końcu zdania To do: 1. dostosować OrderHandler 2. dodać do Waiter OrderHandler 3. zatomatyzować dodawanie MeniuItem

--- src/kck/KCK.java
+++ src/kck/KCK.java
@@ -1,16 +1,23 @@
 package kck;
 
+import kck.order.OrderHandler;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class KCK {
     static String s;
+    static long seed;
 
     public KCK(String s) {
         this.s = s;
+        this.seed = System.currentTimeMillis();
     }
 
     public static void main(String[] args) throws IOException {
@@ -132,9 +139,8 @@ public class KCK {
         }
     }
 
-    private static void dishOfTheDay(String group) throws IOException
+    public static void dishOfTheDay(String group) throws IOException
     {
-
         MenuList menuList = new MenuList();
 
         LinkedList<String> listOf;
@@ -148,29 +154,60 @@ public class KCK {
                 int i = 0;
                 listOf.add(i, tok.getProduct());
                 i = i + 1;
+                // po co ta iteracja?
             }
         }
-
-
-        Random random = new Random();
+        Random random = new Random(seed);
         int index = random.nextInt(listOf.size());
         String item = listOf.get(index);
-        System.out.println("Danie dnia to: " + item);
+        System.out.println(group+" to: " + item);
+    }
 
+    public static String returndishOfTheDay(String group) throws IOException
+    {
+        MenuList menuList = new MenuList();
+
+        LinkedList<String> listOf;
+        listOf = new LinkedList<String>();
+
+        //System.out.println(group);
+        for (MenuList.Menu tok : menuList.getMenu())
+        {
+            if (tok.group.equals(group) == true)
+            {
+                int i = 0;
+                listOf.add(i, tok.getProduct());
+                i = i + 1;
+                // po co ta iteracja?
+            }
+        }
+        Random random = new Random(seed);
+        int index = random.nextInt(listOf.size());
+        String item = listOf.get(index);
+       return  item;
     }
 
 
     private static void proba1(Tokenizer tokenizer) throws IOException
     {
-        String s = "Które dania zawierają mięso";
-        System.out.println("Klient: " + s);
 
-        s = s.toLowerCase();
-        tokenizer.tokenize(s);
-      //  tokenizer.checkproduct();
+        String s = "";
+//        String s = "Dzień dobry";
+//        System.out.println("Klient: " + s);
 
-       Waiter waiter = new Waiter();
-        System.out.println(waiter.talk(tokenizer));
+//        s = s.toLowerCase();
+//        tokenizer.tokenize(s);
+//        tokenizer.checkproduct();
+
+        Waiter waiter = new Waiter();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kelner: Dzień dobry. Czy moge przyjąć zamówinie?");
+        while(!s.equals("Do widzenia")|!s.equals("do widzenia")) {
+            s = scanner.nextLine();
+            tokenizer.tokenize(s.toLowerCase());
+            System.out.println(waiter.talk(tokenizer));
+        }
+
         /*
         System.out.println("Klient zamówił: ");
         for (Tokenizer.Token tok : tokenizer.getTokens()) {
--- src/kck/Waiter.java
+++ src/kck/Waiter.java
@@ -9,16 +9,20 @@ import java.util.List;
  */
 public class Waiter {
 
+    KCK kck = new KCK("");
+
+
 
 String talk(Tokenizer tokenizer) throws IOException {
     String response = "";
     List<Tokenizer.Token> list = new ArrayList<Tokenizer.Token>();
     for(Tokenizer.Token tok: tokenizer.getTokens()){
         list.add(tok);
+        System.out.println(tok.token);
     }
 
     if(list.size()>0) {
-        response = response(list);
+        response = "Kelner: "+response(list);
     }
 
     return response;
@@ -26,23 +30,74 @@ String talk(Tokenizer tokenizer) throws IOException {
 
 String response(List<Tokenizer.Token> tokens) throws IOException {
     String respon = "";
-
     for (int i=0; i<tokens.size(); i++) {
         if (tokens.get(i).token == 0) {
             respon = "Dzień dobry";
-        } else if (tokens.get(i).token == 1 || tokens.get(i).token == 2) {
+        }
+        else if (tokens.get(i).token ==10) {
+            //sama nazwa produktu
+
+            respon = "Zamówiono " + tokens.get(i).boss + ". Czy podać coś jeszcze?";
+
+            // handle order
+
+        }
+        else if (tokens.get(i).token == 1 || tokens.get(i).token == 2) {
             i++;
             if (i < tokens.size() & tokens.get(i).token == 10) {
+                //zamówienie po pełnej nazwie
+
                 respon = "Zamówiono " + tokens.get(i).boss + ". Czy podać coś jeszcze?";
-            } else respon = "Co podać?";
+
+                // handle order
+
+            }
+            else if(i < tokens.size() & tokens.get(i).token == 99) {
+                //zamówienie po niepełnej nazwie
+
+                Tokenizer tokenizer = new Tokenizer();
+                Boolean ordered;
+                ordered = tokenizer.checkproduct(tokens.get(i));
+                if(ordered) {
+
+                    //handle order
+                    respon = "Czy podać coś jeszcze?";
+                }
+            }
+            else if(i<tokens.size() & tokens.get(i).token == 7 ){
+                // zamówiono danie dnia
+                String product = "";
+
+                String word = tokens.get(i).boss.substring(0,1).toUpperCase() + tokens.get(i).boss.substring(1);
+                product = kck.returndishOfTheDay(word);
+
+                // znaleźć token czy nazwa wystarczy?
+
+
+                // order handler
+                respon = "Czy podać coś jeszcze?";
+            }
+            else respon = "Proszę wybrać produkt z menu.";
         }
         else if (tokens.get(i).token == 3 && tokens.get(i+1).token == 4) {
-            KCK kck = new KCK("");
-            i +=2;
-            kck.containsProduct(tokens.get(i).sequence);
-        }
+            //czy z mięsem itp.
 
-        // dodać kolejne
+            i++;
+            kck.containsProduct(tokens.get(i).sequence);
+            respon = "Co podać?";
+        }
+        else if (tokens.get(i).token == 3 && tokens.get(i+1).token == 7) {
+            // danie dnia
+            i++;
+            kck.dishOfTheDay("Zupy");
+            kck.dishOfTheDay("Dania główne");
+            kck.dishOfTheDay("Desery");
+            respon = "Co podać?";
+
+        }
+        else{
+            respon = "Nie zrozuamialem. Proszę inaczej sformułować zdanie.";
+        }
     }
 
     return respon;
