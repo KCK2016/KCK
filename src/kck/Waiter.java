a2aa4724ffe194f70906d922fd5869e351ccf0df
KCK2016 <adrian-1212@wp.pl>
2017-01-05 16:53:35 +0100

    większe pokrycie

--- src/kck/Waiter.java
+++ src/kck/Waiter.java
@@ -38,10 +38,11 @@ a będzie tylko większy
 #######################
  */
 private void response(List<Tokenizer.Token> tokens, OrderHandler orderHandler, MenuList menuList) throws IOException {
-
+    Boolean answer = false;
     for (int i=0; i<tokens.size(); i++) {
         if (tokens.get(i).token == 0) {
             System.out.println("Kelner: Dzień dobry");
+            answer = true;
         }
         else if(tokens.get(i).token == 7){
             // zamówiono danie dnia
@@ -49,17 +50,20 @@ private void response(List<Tokenizer.Token> tokens, OrderHandler orderHandler, M
             String product = kck.returndishOfTheDay(word);
             orderHandler.addToOrder(menuList.findProduct(product));
             System.out.println("Kelner: Czy podać coś jeszcze?");
+            answer = true;
         }
         else if (tokens.get(i).token == 8) {
             //jedynie słowo rachunek
 
             System.out.println("Kelner: "+ orderHandler.buildReceipt());
+            answer = true;
         }
         else if (tokens.get(i).token == 10) {
             //sama nazwa produktu
 
             System.out.println("Kelner: Zamówiono " + tokens.get(i).boss + ". Czy podać coś jeszcze?");
             orderHandler.addToOrder(menuList.findProduct(tokens.get(i).boss));
+            answer = true;
         }
         else if(tokens.get(i).token == 13) {} // tak
         else if(tokens.get(i).token == 14) {} // nie
@@ -70,6 +74,7 @@ private void response(List<Tokenizer.Token> tokens, OrderHandler orderHandler, M
 
                 System.out.println("Kelner: Zamówiono " + tokens.get(i).boss + ". Czy podać coś jeszcze?");
                 orderHandler.addToOrder(menuList.findProduct(tokens.get(i).boss));
+                answer = true;
             }
             else if(i < tokens.size() & tokens.get(i).token == 99) {
                 //zamówienie po niepełnej nazwie
@@ -80,6 +85,7 @@ private void response(List<Tokenizer.Token> tokens, OrderHandler orderHandler, M
 
                     orderHandler.addToOrder(menuList.findProduct(ordered.getProduct()));
                     System.out.println("Kelner: Czy podać coś jeszcze?");
+                    answer = true;
                 }
             }
             else if(i<tokens.size() & tokens.get(i).token == 7 ){
@@ -88,13 +94,18 @@ private void response(List<Tokenizer.Token> tokens, OrderHandler orderHandler, M
                 String product = kck.returndishOfTheDay(word);
                 orderHandler.addToOrder(menuList.findProduct(product));
                 System.out.println("Kelner: Czy podać coś jeszcze?");
+                answer = true;
             }
             else if(i<tokens.size() & tokens.get(i).token == 8){
                 //poproszono o rachunek
 
                 System.out.println("Kelner: "+ orderHandler.buildReceipt());
+                answer = true;
             }
-            else System.out.println("Kelner: Proszę wybrać produkt z menu.");
+            else {
+                System.out.println("Kelner: Proszę wybrać produkt z menu.");
+                answer = true;
+            }
         }
         else if (tokens.get(i).token == 3 && tokens.get(i+1).token == 4) {
             //czy z mięsem itp.
@@ -102,12 +113,14 @@ private void response(List<Tokenizer.Token> tokens, OrderHandler orderHandler, M
             i +=2;
             kck.containsProduct(tokens.get(i).sequence);
             System.out.println("Kelner: Co podać?");
+            answer = true;
         }
         else if (tokens.get(i).token == 3 && tokens.get(i+1).token == 15) {
             //czy z mięsem itp.
             i++;
             kck.containsProduct(tokens.get(i).boss);
             System.out.println("Kelner: Co podać?");
+            answer = true;
         }
         else if (tokens.get(i).token == 3 && tokens.get(i+1).token == 7) {
             // danie dnia
@@ -116,10 +129,15 @@ private void response(List<Tokenizer.Token> tokens, OrderHandler orderHandler, M
             kck.dishOfTheDay("Dania główne");
             kck.dishOfTheDay("Desery");
             System.out.println("Kelner: Czy coś podać?");
+            answer = true;
         }
-        else{
-            System.out.println("Kelner: Nie zrozuamialem. Proszę inaczej sformułować zdanie.");
+        else if(tokens.get(i).token == 17) {
+            System.out.println("Kelner: Polecam danie dnia.");
+            answer = true;
         }
+    }
+    if(!answer){
+        System.out.println("Kelner: Nie zrozuamialem. Proszę inaczej sformułować zdanie.");
     }
 }
 
